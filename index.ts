d23d0b1da779b72a01f54b4aa29150936b808b06
sujin921.kim <sujin921.kim@gmail.com>
2025-07-02 21:20:43 +0900

    init: init package

---
+++ index.ts
@@ -0,0 +1,13 @@
+const multiply = (a: number, b: number): number => {
+  return a * b;
+};
+
+const divide = (a: number, b: number): number => {
+  return a / b;
+};
+
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+export { multiply, divide, add };
