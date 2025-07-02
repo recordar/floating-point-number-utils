80934fcbf5b3a6feb7bea615881c8163f154a3d5
sujin921.kim <sujin921.kim@gmail.com>
2025-07-02 22:06:20 +0900

    fix: 누락된 build 추가

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,11 @@
+import Big from "big.js";
 const multiply = (a, b) => {
-    return a * b;
+    return new Big(a).times(b).toNumber();
 };
 const divide = (a, b) => {
-    return a / b;
+    return new Big(a).div(b).toNumber();
 };
 const add = (a, b) => {
-    return a + b;
+    return new Big(a).plus(b).toNumber();
 };
 export { multiply, divide, add };
--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
 import Big from "big.js";
 
 const multiply = (a: number, b: number) => {
-  return Big(a).times(b).toNumber();
+  return new Big(a).times(b).toNumber();
 };
 
 const divide = (a: number, b: number) => {
-  return Big(a).div(b).toNumber();
+  return new Big(a).div(b).toNumber();
 };
 
 const add = (a: number, b: number) => {
-  return Big(a).plus(b).toNumber();
+  return new Big(a).plus(b).toNumber();
 };
 
 export { multiply, divide, add };
