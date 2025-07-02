0095d3d090dce88af740d225b7d71ebe7910d697
sujin921.kim <sujin921.kim@gmail.com>
2025-07-02 22:03:09 +0900

    feat: big.js 적용

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
+import Big from "big.js";
+
 const multiply = (a: number, b: number) => {
-  return a * b;
+  return Big(a).times(b).toNumber();
 };
 
 const divide = (a: number, b: number) => {
-  return a / b;
+  return Big(a).div(b).toNumber();
 };
 
 const add = (a: number, b: number) => {
-  return a + b;
+  return Big(a).plus(b).toNumber();
 };
 
 export { multiply, divide, add };
