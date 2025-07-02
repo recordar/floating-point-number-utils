68e3096b7a37c8988698a22a7046c96d042dbf6d
sujin921.kim <sujin921.kim@gmail.com>
2025-07-02 21:45:28 +0900

    fix: fix module

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+declare const multiply: (a: number, b: number) => number;
+declare const divide: (a: number, b: number) => number;
+declare const add: (a: number, b: number) => number;
+export { multiply, divide, add };
---
+++ dist/index.js
@@ -0,0 +1,10 @@
+const multiply = (a, b) => {
+    return a * b;
+};
+const divide = (a, b) => {
+    return a / b;
+};
+const add = (a, b) => {
+    return a + b;
+};
+export { multiply, divide, add };
